93ea8cbe582dbcd9202e2841b2654ca4995d5acf
bosque-juan <juan_bosque@hotmail.com>
2025-12-17 10:49:30 -0300

    Add entities for Invoice(bill)

---
+++ src/AirLineMetrics.Domains/Entities/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineMetrics.Domain.Models
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+
+        public double PaymentAmount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public int PaymentMethodId { get; set; }
+    }
+}
