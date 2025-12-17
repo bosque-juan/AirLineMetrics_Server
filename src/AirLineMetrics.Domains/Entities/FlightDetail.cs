8a8e06c2ac01647edc3dd000692f5a3d3a20f3d2
bosque-juan <juan_bosque@hotmail.com>
2025-12-17 10:48:52 -0300

    Add Entitites for Flight Details

---
+++ src/AirLineMetrics.Domains/Entities/FlightDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineMetrics.Domain.Models
+{
+    public class FlightDetail
+    {
+        public int FlightDetailId { get; set; }
+        public decimal FinlaPrice { get; set; }
+        public string Description { get; set; }
+
+        public int PassengerId { get; set; }
+        public int FlightId { get; set; }
+        public int SeatId { get; set; }
+
+    }
+}
---
+++ src/AirLineMetrics.Domains/Entities/SeatType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineMetrics.Domain.Models
+{
+    public class SeatType
+    {
+        public int SeatTypeId { get; set; }
+        public string SeatName { get; set; }
+        public string Description { get; set; }
+        public double AddedSeatTypePrice { get; set; }
+    }
+}
