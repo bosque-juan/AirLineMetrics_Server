89d4c32bb177ccec2c35fd9a95ebf3a799d9d564
bosque-juan <juan_bosque@hotmail.com>
2025-12-17 10:30:48 -0300

    Entities for passengers

---
+++ src/AirLineMetrics.Domains/Entities/DocumentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirLineMetrics.Domain.Models;
+
+namespace AirLIneDbContext.Models
+{
+    public class DocumentType
+    {
+        public int DocumentTypeId { get; set; }
+
+        public string DocumentTypeName { get; set; }
+
+        public virtual ICollection<Passenger> passengers {get;set;} = new List<Passenger>();
+    }
+}
---
+++ src/AirLineMetrics.Domains/Entities/Passenger.cs
@@ -0,0 +1,23 @@
+﻿//using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirLineMetrics.Domain.Models
+{
+   public class Passenger
+    {
+        public int PassengerId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+        public int DocumentTypeId { get; set; }
+        public int ClientTypeId {  get; set; }
+        public string Street {  get; set; }
+        public string Email { get; set; }
+        public int MobileNumber { get; set; }
+
+        public int StateId { get; set; }
+    }
+}
